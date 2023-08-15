a4c3a13805f9733bce42b575fa988a664c731bf7
Carlyon Jones-Armour <95041187+TroubledDreamer@users.noreply.github.com>
2023-08-15 20:43:33 -0300

    implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import RecentBlock from "./components/RecentBLock";
 import UploadBlock from "./components/UploadBlock";
 import ListGroup from "./components/UploadBlock";
 import './app.css';
+import { Link } from "react-router-dom";
+
+
 
 function App() {
 
   return (
     <div>
+      <Link to={"/Library"}>dsmdsdn</Link>
       <UploadBlock />
       <RecentBlock />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
+import LibraryPage from './pages/LibraryPage.jsx';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Library",
+    element: <LibraryPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
